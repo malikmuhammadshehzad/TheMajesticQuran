3ab713cf3ebebf0444b8decedf7a3c47bd2d650d
malikmuhammadshehzad <malikshehzad1534@gmail.com>
2023-12-12 19:09:19 +0500

    Player with modal

--- src/screen/player/index.js
+++ src/screen/player/index.js
@@ -10,6 +10,8 @@ import {
   Image,
   FlatList,
   Animated,
+  Modal,
+  Pressable,
 } from 'react-native';
 import TrackPlayer, {
   Capability,
@@ -22,6 +24,7 @@ import TrackPlayer, {
 } from 'react-native-track-player';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import Ionicons from 'react-native-vector-icons/Ionicons';
+import { COLORS, ICONS } from '../../assets';
 /// my Data 
 // import { useDispatch } from 'react-redux';
 // import { PlayerData } from '../../Redux/Reducers/PlayerReducer';
@@ -40,6 +43,13 @@ import Ionicons from 'react-native-vector-icons/Ionicons';
 // nomal player 
 const tracks = [
   {
+    id: 2,
+    title: 'Awful',
+    artist: 'pan',
+    artwork:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fdomf5oio6qrcr.cloudfront.net%2Fmedialibrary%2F8718%2Fbrain-music-health-memory.jpg&tbnid=70poib9oaptTMM&vet=12ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygLegUIARCJAQ..i&imgrefurl=https%3A%2F%2Fwww.health.harvard.edu%2Fblog%2Fhealing-through-music-201511058556&docid=NiquLkcrTHWSWM&w=1518&h=1265&q=music&ved=2ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygLegUIARCJAQ',
+    url: 'https://doctorsforhealth.co.uk/mp3/Arabic_sudais/001.mp3',
+  },
+  {
     id: 1,
     title: 'Awful',
     artist: 'josh',
@@ -47,22 +57,7 @@ const tracks = [
     artwork:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.copyright.gov%2Fmusic-modernization%2Fimages%2Flicensing-interactive-streams.jpg&tbnid=duDEND4DZanofM&vet=12ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygRegUIARCWAQ..i&imgrefurl=https%3A%2F%2Fwww.copyright.gov%2Fmusic-modernization%2F&docid=uXy9p7Umlf8lAM&w=5000&h=3337&q=music&ved=2ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygRegUIARCWAQ',
   },
   {
-    id: 2,
-    title: 'Awful',
-    artist: 'josh pan',
-    artwork:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fdomf5oio6qrcr.cloudfront.net%2Fmedialibrary%2F8718%2Fbrain-music-health-memory.jpg&tbnid=70poib9oaptTMM&vet=12ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygLegUIARCJAQ..i&imgrefurl=https%3A%2F%2Fwww.health.harvard.edu%2Fblog%2Fhealing-through-music-201511058556&docid=NiquLkcrTHWSWM&w=1518&h=1265&q=music&ved=2ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygLegUIARCJAQ',
-    url: 'https://commondatastorage.googleapis.com/codeskulptor-demos/DDR_assets/Kangaroo_MusiQue_-_The_Neverwritten_Role_Playing_Game.mp3',
-
-  },
-  {
     id: 3,
-    title: 'Awful',
-    artist: 'pan',
-    artwork:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fdomf5oio6qrcr.cloudfront.net%2Fmedialibrary%2F8718%2Fbrain-music-health-memory.jpg&tbnid=70poib9oaptTMM&vet=12ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygLegUIARCJAQ..i&imgrefurl=https%3A%2F%2Fwww.health.harvard.edu%2Fblog%2Fhealing-through-music-201511058556&docid=NiquLkcrTHWSWM&w=1518&h=1265&q=music&ved=2ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygLegUIARCJAQ',
-    url: 'https://doctorsforhealth.co.uk/mp3/Arabic_sudais/001.mp3',
-  },
-  {
-    id: 4,
     title: 'Awful',
     artwork:'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.copyright.gov%2Fmusic-modernization%2Fimages%2Flicensing-interactive-streams.jpg&tbnid=duDEND4DZanofM&vet=12ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygRegUIARCWAQ..i&imgrefurl=https%3A%2F%2Fwww.copyright.gov%2Fmusic-modernization%2F&docid=uXy9p7Umlf8lAM&w=5000&h=3337&q=music&ved=2ahUKEwirkcSO2YmDAxUvUKQEHQLbB14QMygRegUIARCWAQ',
     artist: 'j pan',
@@ -80,6 +75,7 @@ TrackPlayer.updateOptions({
 });
 
 const Player = () => {
+  const [isModalVisible, setModalVisible] = useState(false);
   // nomal player 
   const setUpTrackPlayer = async () => {
     try {
@@ -95,10 +91,15 @@ const Player = () => {
     setUpTrackPlayer();
     return () => TrackPlayer.destroy();
   }, []);
+  const toggleModal = () => {
+    console.log('Toggling modal. Current state:', isModalVisible);
+    setModalVisible(!isModalVisible);
+  };
  
   return (   
-  
-    <SafeAreaView style={style.container}>
+  <>
+  <View style={style.container}>
+ 
       {/* music player section */}
       <View style={style.mainContainer}>
            {/* music control */}
@@ -109,14 +110,14 @@ const Player = () => {
           <TouchableOpacity onPress={() => TrackPlayer.pause()}>
             <FontAwesome
               name="play"
-              size={75}
+              size={40}
               color="#FFD369"
             />
           </TouchableOpacity>
           <TouchableOpacity onPress={() => TrackPlayer.play()}>
             <FontAwesome
               name="pause"
-              size={75}
+              size={35}
               color="#FFD369"
             />
           </TouchableOpacity>
@@ -133,50 +134,52 @@ const Player = () => {
       {/* bottom section */}
       <View style={style.bottomSection}>
         <View style={style.bottomIconContainer}>
-          <TouchableOpacity >
+          <TouchableOpacity  onPress={toggleModal} >
             {/* <MaterialCommunityIcons
               name={`${repeatIcon()}`}
               size={30}
               color={repeatMode !== 'off' ? '#FFD369' : '#888888'}
             /> */}
+            <ICONS.manIcon color={"white"} />
           </TouchableOpacity>
         </View>
       </View>
-    </SafeAreaView>
+    </View>
+    <View styles={{flex:1}} >
+
+   
+     {isModalVisible== true ? <Modal isVisible={false} >
+      <View style={style.modalMain} >
+       <Text styles={{color:COLORS.black , fontSize:27 }} >
+            Mishary bin Rashid
+           </Text>
+       <Text styles={{color:COLORS.black ,fontSize:27 }} >
+            Abdul Rehman Al-sudais
+           </Text>
+       <Text styles={{color:COLORS.black ,fontSize:27 }} >
+           Abdul Basit
+           </Text>
+           <Pressable onPress={toggleModal} >
+            <Text styles={{color:COLORS.black ,fontSize:27 }} >
+            back to Player
+            </Text>        
+           </Pressable>
+      </View>
+      </Modal> : (
+        null
+      )  }
+  </View>
+    </>
   );
 };
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: 'black',
-//   },
-//   btn: {
-//     backgroundColor: '#ff0044',
-//     padding: 15,
-//     borderRadius: 10,
-//     margin: 10,
-//     width: 160,
-//   },
-//   text: {
-//     fontSize: 30,
-//     color: 'white',
-//     textAlign: 'center',
-//   },
-//   row: {
-//     flexDirection: 'row',
-//     marginBottom: 20,
-//   },
-// });
+ 
 
 export default Player;
 
 const style = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#222831',
+    backgroundColor:COLORS.navyBlue
   },
   mainContainer: {
     flex: 1,
@@ -185,18 +188,16 @@ const style = StyleSheet.create({
   },
   bottomSection: {
     borderTopColor: '#393E46',
-    borderWidth: 1,
-    // width: width,
+    backgroundColor:COLORS.white,
+    borderWidth: 1,   
     alignItems: 'center',
     paddingVertical: 15,
   },
-
   bottomIconContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     width: '80%',
   },
-
   mainWrapper: {
     // width: width,
     justifyContent: 'center',
@@ -261,4 +262,10 @@ const style = StyleSheet.create({
     marginTop: 15,
     width: '60%',
   },
+  modalMain:{
+    width:'80%',
+     height:200,
+     borderRadius: 12,
+      padding: 12 
+  }
 });
